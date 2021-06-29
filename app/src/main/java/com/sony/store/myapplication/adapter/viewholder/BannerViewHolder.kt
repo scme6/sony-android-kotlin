4179b41fd2d806ce573a421b3d40fcbc7f1e6286
alan.wang <alan.wang@sact-digital.com>
2021-06-29 17:29:56 +0800

    首次提交

---
+++ app/src/main/java/com/sony/store/myapplication/adapter/viewholder/BannerViewHolder.kt
@@ -0,0 +1,16 @@
+package com.sony.store.myapplication.adapter.viewholder
+
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_test_banner.view.*
+
+class BannerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bindTo(){
+
+
+
+
+    }
+}
