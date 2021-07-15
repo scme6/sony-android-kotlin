67fa755d94d84f93b9eac65edaf5c53ba2d12fcb
qiangfei <qiangfeili@gmail.com>
2021-07-15 18:02:41 +0800

    通知

--- app/src/main/java/com/sony/store/myapplication/MainActivity.kt
+++ app/src/main/java/com/sony/store/myapplication/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.sony.store.myapplication
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.TextView
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.sony.store.myapplication.base.BaseActivity
+import com.sony.store.myapplication.ui.activity.SearchActivity
 import com.sony.store.myapplication.ui.activity.WebViewActivity
 import com.sony.store.myapplication.ui.fragment.CategoryFragment
 import com.sony.store.myapplication.ui.fragment.HomeFragment
 import com.sony.store.myapplication.ui.fragment.SettingFragment
 import com.sony.store.myapplication.utils.UIUtil.dip2px
-import com.sony.store.myapplication.widget.DownloadProgress
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -35,6 +42,7 @@ class MainActivity : BaseActivity() {
                 supportFragmentManager.findFragmentByTag(TAG_SETTING) as SettingFragment?
         }
         initView()
+        sendNotification()
     }
 
     private fun initView() {
@@ -160,6 +168,108 @@ class MainActivity : BaseActivity() {
 //        }
         dialog.show()
     }
+
+
+    private fun sendNotification(){
+       val intent=PendingIntent.getActivity(this, 123, Intent(this,SearchActivity::class.java), PendingIntent.FLAG_UPDATE_CURRENT)
+       notification("title","content",80,intent)
+    }
+    private fun notification(title: String, content: String, progress: Int, intent: PendingIntent) {
+        val sys = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        var builder : NotificationCompat.Builder?=null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "default";
+            val channelName = "默认通知";
+            val mChannel = NotificationChannel(channelId,channelName,NotificationManager.IMPORTANCE_HIGH)
+            //  mChannel.setDescription(description);
+            //  mChannel.enableLights(true);
+            //  mChannel.setLightColor(Color.RED);
+            //  mChannel.enableVibration(true);
+            //  mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            //  mChannel.setDescription(description);
+            //  mChannel.enableLights(true);
+            //  mChannel.setLightColor(Color.RED);
+            //  mChannel.enableVibration(true);
+            //  mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setSound(null, null)
+            mChannel.importance = IMPORTANCE_LOW
+            sys.createNotificationChannel(mChannel)
+            builder = NotificationCompat.Builder(this, channelId)
+        }
+         builder=NotificationCompat.Builder(this,"default")
+        //标题
+        builder.setContentTitle(title)
+        //文本内容
+        builder.setContentText(content)
+        //小图标
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+        //设置大图标，未设置时使用小图标代替，拉下通知栏显示的那个图标
+        // 设置大图片
+        // BitmpFactory.decodeResource(Resource res,int id)
+        // 根据给定的资源Id解析成位图
+        // builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        if (progress > 0 && progress < 100) {
+            //一种是有进度刻度的（false）,
+            // 一种是循环流动的（true）
+            // 设置为false，表示刻度，
+            // 设置为true，表示流动
+            builder.setProgress(100, 0, false)
+
+        } else {
+            //0,0,false,可以将进度条隐藏
+            builder.setProgress(0, 0, false)
+            builder.setContentText("**完成");
+        }
+        //设置点击信息后自动清除通知
+        builder.setAutoCancel(true)
+        //通知的时间
+        builder.setWhen(System.currentTimeMillis())
+        //设置点击信息后的跳转（意图）
+        builder.setContentIntent(intent)
+        builder.setOnlyAlertOnce(true)
+        sys.notify(1, builder.build())
+
+        lifecycleScope.launch {
+            repeat(100){
+                delay(100)
+                builder.setProgress(100, it, false)
+                sys.notify(1, builder.build())
+
+//                builder.setProgress(100, it, false)
+//                sys.notify(it, builder.build())
+            }
+        }
+
+
+
+//        val manager=getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//        //需添加的代码
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+//            val channelId = "default";
+//            val channelName = "默认通知";
+//            manager.createNotificationChannel( NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH));
+//        }
+//        //
+//        val notification = NotificationCompat.Builder(this,"default")
+//            .setContentTitle("This is content title")
+//            .setContentText("this is content text")
+//            .setWhen(System.currentTimeMillis())
+//            .setSmallIcon(R.mipmap.ic_launcher)
+//            .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+//            .build();
+//        manager.notify(1,notification);
+
+
+
+
+
+
+
+
+
+    }
+
+
 
 
 }
